c57ace31c475c0dfbcbccfe5af06d9c2edf838e5
molero3111 <molero3111@gmail.com>
2025-06-08 21:23:39 -0300

    Add allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "django-insecure-#)l-vwlx0(-3g_je&rb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG", "True").lower() in ("true", "1", "yes")
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",") if os.getenv("ALLOWED_HOSTS") else []
 
 # Application definition
 
